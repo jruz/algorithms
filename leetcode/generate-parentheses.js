a6e5b9f67e0da7b9ed25f29b9d06929121353093
Javier Ruz <javi@jruz.io>
2019-10-28 17:35:21 +0100

    CI (#11)
    * CI
    
    * Fix prettier

--- leetcode/generate-parentheses.js
+++ leetcode/generate-parentheses.js
@@ -1,11 +1,11 @@
-export const parens = n => {
+export const parens = (n) => {
   const res = [];
   const generate = (s, left, right) => {
     if (left < n) {
-      generate(s + '(', left + 1, right);
+      generate(`${s}(`, left + 1, right);
     }
     if (right < left) {
-      generate(s + ')', left, right + 1);
+      generate(`${s})`, left, right + 1);
     }
     if (s.length === n * 2) res.push(s);
   };
